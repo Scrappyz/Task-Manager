3c0b1cac83f8f206d6d57520b0ef186d08595484
Scrappyz <ghostmic3000@gmail.com>
2024-11-28 17:42:09 +0800

    Remade

--- lib/main.dart
+++ lib/main.dart
@@ -1,130 +1,163 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const TicTacToeApp());
+  runApp(TaskManagerApp());
 }
 
-class TicTacToeApp extends StatelessWidget {
-  const TicTacToeApp({Key? key}) : super(key: key);
-
+class TaskManagerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Tic Tac Toe',
-      home: const TicTacToeGame(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/grid': (context) => CategoryScreen(),
+        '/details': (context) => TaskDetailsScreen(),
+      },
     );
   }
 }
 
-class TicTacToeGame extends StatefulWidget {
-  const TicTacToeGame({Key? key}) : super(key: key);
-
-  @override
-  _TicTacToeGameState createState() => _TicTacToeGameState();
-}
-
-class _TicTacToeGameState extends State<TicTacToeGame> {
-  List<String> board = List.filled(9, '');
-  bool isXTurn = true;
-  String winner = '';
-
-  void _resetGame() {
-    setState(() {
-      board = List.filled(9, '');
-      isXTurn = true;
-      winner = '';
-    });
-  }
-
-  void _makeMove(int index) {
-    if (board[index] == '' && winner == '') {
-      setState(() {
-        board[index] = isXTurn ? 'X' : 'O';
-        isXTurn = !isXTurn;
-        winner = _checkWinner();
-      });
-    }
-  }
-
-  String _checkWinner() {
-    const winningPatterns = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (var pattern in winningPatterns) {
-      String a = board[pattern[0]];
-      String b = board[pattern[1]];
-      String c = board[pattern[2]];
-      if (a != '' && a == b && a == c) {
-        return a;
-      }
-    }
-
-    if (!board.contains('')) {
-      return 'Draw';
-    }
-
-    return '';
-  }
+class HomeScreen extends StatelessWidget {
+  final List<String> tasks = List.generate(10, (index) => 'Task $index');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Tic Tac Toe'),
+        title: Text('Tasks Manager'),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            winner == ''
-                ? "Player ${isXTurn ? 'X' : 'O'}'s Turn"
-                : (winner == 'Draw' ? 'It\'s a Draw!' : 'Player $winner Wins!'),
-            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 20),
-          // Constrain the size of the grid
-          AspectRatio(
-            aspectRatio: 1, // Ensures a square grid
-            child: GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Text(
+                'Menu',
+                style: TextStyle(color: Colors.white, fontSize: 24),
               ),
-              itemCount: 9,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () => _makeMove(index),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                    ),
-                    child: Center(
-                      child: Text(
-                        board[index],
-                        style: const TextStyle(fontSize: 36),
-                      ),
-                    ),
-                  ),
-                );
+              decoration: BoxDecoration(color: Colors.blue),
+            ),
+            ListTile(
+              title: Text('View Categories'),
+              onTap: () {
+                Navigator.pushNamed(context, '/grid');
               },
             ),
+          ],
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: Icon(Icons.check_circle_outline),
+            title: Text(tasks[index]),
+            onTap: () {
+              Navigator.pushNamed(context, '/details',
+                  arguments: tasks[index]);
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Add new tasks feature coming soon!')),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class CategoryScreen extends StatelessWidget {
+  final List<String> categories = [
+    'Work',
+    'Personal',
+    'Health',
+    'Shopping',
+    'Learning',
+    'Hobbies',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Categories')),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('${categories[index]} selected')),
+              );
+            },
+            child: Card(
+              color: Colors.blue[100],
+              child: Center(
+                child: Text(
+                  categories[index],
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class TaskDetailsScreen extends StatefulWidget {
+  @override
+  _TaskDetailsScreenState createState() => _TaskDetailsScreenState();
+}
+
+class _TaskDetailsScreenState extends State<TaskDetailsScreen> {
+  bool _completed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final String taskName = ModalRoute.of(context)!.settings.arguments as String;
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Task Details')),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              _completed = !_completed;
+            });
+          },
+          child: AnimatedContainer(
+            duration: Duration(seconds: 1),
+            padding: EdgeInsets.all(20),
+            width: _completed ? 250 : 200,
+            height: _completed ? 100 : 150,
+            decoration: BoxDecoration(
+              color: _completed ? Colors.green[200] : Colors.red[200],
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Center(
+              child: Text(
+                _completed
+                    ? '$taskName Completed!'
+                    : 'Mark $taskName as Complete',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
           ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: _resetGame,
-            child: const Text('Reset Game'),
-          ),
-        ],
+        ),
       ),
     );
   }
